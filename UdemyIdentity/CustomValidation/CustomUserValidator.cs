502d8ebc7e1839838148dcf136144bc4ff203626
Vahap Gozenelioglu <vgozenelioglu@curiesoft.com>
2023-01-24 00:23:25 +0300

    Add default and custom user validation

---
+++ UdemyIdentity/CustomValidation/CustomUserValidator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UdemyIdentity.Models;
+
+namespace UdemyIdentity.CustomValidation
+{
+    public class CustomUserValidator : IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            List<IdentityError> errors = new List<IdentityError>();
+
+            var digits = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            
+            foreach(var digit in digits)
+            {
+                if (user.UserName[0].ToString() == digit)
+                {
+                    errors.Add(new IdentityError
+                    {
+                        Code = "UsernameStartsWithDigit",
+                        Description = "Username cannot start with digit"
+                    });
+                }
+            }
+
+            if(errors.Count == 0)
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+
+            return Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+        }
+    }
+}
--- UdemyIdentity/Startup.cs
+++ UdemyIdentity/Startup.cs
@@ -41,12 +41,15 @@ namespace UdemyIdentity
 
             services.AddIdentity<AppUser, AppRole>(opts =>
             {
+                opts.User.RequireUniqueEmail = true;
+                opts.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+
                 opts.Password.RequiredLength = 4;
                 opts.Password.RequireNonAlphanumeric = false;
                 opts.Password.RequireLowercase = false;
                 opts.Password.RequireUppercase = false;
                 opts.Password.RequireDigit = false;
-            }).AddPasswordValidator<CustomPasswordValidator>().AddEntityFrameworkStores<AppIdentityDbContext>();
+            }).AddPasswordValidator<CustomPasswordValidator>().AddUserValidator<CustomUserValidator>().AddEntityFrameworkStores<AppIdentityDbContext>();
 
 
             services.AddMvc(option => option.EnableEndpointRouting = false);
