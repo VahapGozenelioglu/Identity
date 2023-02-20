e12f4d82a59da093f45537360c75d15ad7653cfc
Vahap Gozenelioglu <vgozenelioglu@curiesoft.com>
2023-02-21 00:36:09 +0300

    Change password operation

--- UdemyIdentity/Controllers/MemberController.cs
+++ UdemyIdentity/Controllers/MemberController.cs
@@ -27,5 +27,48 @@ namespace UdemyIdentity.Controllers
 
             return View(userViewModel);
         }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ChangePassword(PasswordChangeViewModel passwordChangeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.FindByNameAsync(User.Identity.Name).Result;
+
+                bool exist = userManager.CheckPasswordAsync(user, passwordChangeViewModel.OldPassword).Result;
+
+                if (exist)
+                {
+                    IdentityResult result = userManager.ChangePasswordAsync(user, passwordChangeViewModel.OldPassword, passwordChangeViewModel.NewPassword).Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.UpdateSecurityStampAsync(user);
+
+                        signInManager.SignOutAsync();
+                        signInManager.PasswordSignInAsync(user, passwordChangeViewModel.NewPassword, true, false);
+                        ViewBag.success = "true";
+                    }
+                    else
+                    {
+                        foreach(var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Your old password is wrong");
+                }
+            }
+
+            return View();
+        }
     }
 }
---
+++ UdemyIdentity/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyIdentity.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Password should contain at least 4 characters")]
+        [Display(Name = "Your old password")]
+        [Required(ErrorMessage = "Old password is required")]
+        public string OldPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Password should contain at least 4 characters")]
+        [Display(Name = "Your new password")]
+        [Required(ErrorMessage = "New password is required")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Password should contain at least 4 characters")]
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Password confirmation is required")]
+        [Compare("NewPassword", ErrorMessage = "Passwords does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
